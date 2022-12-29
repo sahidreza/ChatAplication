3a4bc591280d969262675659ccccf4a491fbf787
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-29 21:01:32 +0530

    Initial Commit

---
+++ Chat Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat Application
+//
+//  Created by Sahid Reza on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
